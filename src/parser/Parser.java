5af9bfb31c061fd93a6f35c6460bd7b8f0732cec
Ben Reisner <Benjamin.Reisner@duke.edu>
2014-10-10 00:07:26 -0400

    Added Minkwon's parser

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -25,8 +25,7 @@ public class Parser {
     /**
      * Parses a string input and constructs a collection of executable commands.
      * 
-     * @param input
-     *            : String to parse.
+     * @param input : String to parse.
      * @return Collection of commands to execute.
      */
     public Collection<Command> parseInput (String parseInput) {
@@ -37,84 +36,63 @@ public class Parser {
             if (MasterController.myCommandMap.containsKey(input)) {
                 try {
                     commandStack.add(MasterController.myCommandMap.get(input));
-                }
-                catch (Exception e) {
+                } catch (Exception e) {
                     return throwError(e);
                 }
-            }
-            else {
+            } else {
                 try {
                     double parameter = Double.parseDouble(input);
                     parameterStack.add(parameter);
-                }
-                catch (Exception e) {
+                } catch (Exception e) {
                     return throwError(e);
                 }
             }
         }
+
         while (!commandStack.isEmpty()) {
             String commandName = commandStack.pop();
             Class<?> cl;
             Command command;
+
             try {
                 cl = Class.forName(commandName);
-                if (cl.getInterfaces()[0].getName() == "commands.TurtleCommand") {
-                    try {
-                        command = (Command) cl.getConstructor().newInstance();
-                        System.out.println(command.getNumParameters());
-                        if (command.getNumParameters() == 0) {
-                            commandsToExecute.add(command);
-                        }
-                        if (command.getNumParameters() == 1) {
-                            ((OneInputCommand) command).setParameters(parameterStack.pop());
-                            commandsToExecute.add(command);
-                        }
-                        if (command.getNumParameters() == 2) {
-                            ((TwoInputCommand) command).setParameters(parameterStack.pop(), parameterStack.pop());
-                            commandsToExecute.add(command);
-                        }
-                        if (command.getNumParameters() == 3) {
-                            ((ThreeInputCommand) command).setParameters(parameterStack.pop(), parameterStack.pop(), parameterStack.pop());
-                            commandsToExecute.add(command);
-                        }
+                try {
+                    command = (Command) cl.getConstructor().newInstance();
+                    if (command.getNumParameters() == 1) {
+                        ((OneInputCommand) command).setParameters(parameterStack.pop());
                     }
-                    catch (Exception e) {
-                        System.out.println("OH YEAH");
-                        return throwError(e);
+                    if (command.getNumParameters() == 2) {
+                        ((TwoInputCommand) command).setParameters(parameterStack.pop(),
+                                parameterStack.pop());
                     }
+                    if (command.getNumParameters() == 3) {
+                        ((ThreeInputCommand) command).setParameters(parameterStack.pop(),
+                                parameterStack.pop(), parameterStack.pop());
+                    }
+                    if (cl.getInterfaces().length > 0
+                            && cl.getInterfaces()[0].getName().equals("commands.TurtleCommand")) {
+                        commandsToExecute.add(command);
+                    } else {
+                        parameterStack.add(command.executeCommand());
+                    }
+                } catch (Exception e) {
+                    return throwError(e);
                 }
-                else if (cl.getInterfaces()[0].getName() != "commands.TurtleCommand") {
-                    try {
-                        command = (Command) cl.getConstructor().newInstance();
-                        if (command.getNumParameters() == 1) {
-                            ((OneInputCommand) command).setParameters(parameterStack.pop());
-                            parameterStack.add(command.executeCommand());
-                        }
-                        if (command.getNumParameters() == 2) {
-                            ((TwoInputCommand) command).setParameters(parameterStack.pop(), parameterStack.pop());
-                            parameterStack.add(command.executeCommand());
-                        }
-                        if (command.getNumParameters() == 3) {
-                            ((ThreeInputCommand) command).setParameters(parameterStack.pop(), parameterStack.pop(), parameterStack.pop());
-                            parameterStack.add(command.executeCommand());
-                        }
-                    }
-                    catch (Exception e) {
-                        System.out.println("OH NO");
-                        return throwError(e);
-                    }
-                }
-            }
-            catch (ClassNotFoundException e) {
-                System.out.println("OH");
+            } catch (ClassNotFoundException e) {
                 return throwError(e);
             }
+
         }
-        for(Command command : commandsToExecute){
-            System.out.println(command.getClass().toString());
-            Turtle turtle = new Turtle(0, 0, new ViewPanel());
-            turtle.updateTurtle((TurtleCommand)command);
+        //I need to throw an error for this case
+        //"fd sum 5 sum 5 3 3"
+        if(parameterStack.size() >0){
+            System.out.println("whatwhatwhat");
         }
+//        for (Command command : commandsToExecute) {
+//            System.out.println(command.getClass().toString());
+//            Turtle turtle = new Turtle(0, 0, new ViewPanel());
+//            turtle.updateTurtle((TurtleCommand) command);
+//        }
         return commandsToExecute;
     }
 
