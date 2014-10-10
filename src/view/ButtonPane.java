13262edf621f64b9a128fc548c7c7fb9cba31b7a
Arihant Jain <aj148@users.noreply.github.com>
2014-10-10 13:09:44 -0400

    Added Buttons

--- src/view/ButtonPane.java
+++ src/view/ButtonPane.java
@@ -1,21 +1,35 @@
 package view;
 
+
+
+import java.io.File;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ToolBar;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 
 
 public class ButtonPane extends Pane {
 
     private final ToolBar myToolBar = new ToolBar();
-    private ComboBox myComboBox = new ComboBox();
-    private Button backgroundColorButton;
-    private Button penColorButton;
+    private ComboBox<Button> myComboBox = new ComboBox();
+    private HBox myHbox = new HBox();
+    private Button toggleReferenceGrid;
+    private Button Help;
+    private Button myChooseFileButton;
     private CommandString myCommandString;
     private String myCurrent;
+    private ColorPicker colorPicker;
+    private File file;
+    private ColorPicker colorPicker2;
 
     public ButtonPane (CommandString cs) {
         myCommandString = cs;
@@ -23,11 +37,16 @@ public class ButtonPane extends Pane {
     }
 
     public void createPropertiesMenu () {
-        backgroundColorButton = makeButton("Background Color", event -> backgroundColor());
-        penColorButton = makeButton("Pen Color", event -> changePenColor());
-        myComboBox.getItems().addAll(backgroundColorButton, penColorButton);
-        myToolBar.getItems().add(myComboBox);
+        colorPicker = makeColorPicker("Background Color", event -> backgroundColor());
+        
+        colorPicker2 = makeColorPicker("Pen Color", event -> changePenColor());
+        toggleReferenceGrid = makeButton("Toggle Grid", event -> toggleGrid());
+        Help = makeButton("Help", event -> help());
+        myChooseFileButton = makeButton("Choose Image",event -> doChoose());
+        myHbox.getChildren().addAll(colorPicker,colorPicker2,toggleReferenceGrid,Help,myChooseFileButton);
+        myToolBar.getItems().add(myHbox);
     }
+    
 
     @Override
     public BorderPane addPane (BorderPane p) {
@@ -42,17 +61,49 @@ public class ButtonPane extends Pane {
         result.setOnAction(handler);
         return result;
     }
+    
+    private void doChoose() {
+    	final Label labelFile = new Label();
+		FileChooser fileChooser = new FileChooser();
+        //Set extension filter
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Image Files (*.png)", "*.png");
+        fileChooser.getExtensionFilters().add(extFilter);
+        //Show open file dialog
+        file = fileChooser.showOpenDialog(null);
+        labelFile.setText(file.getPath());
+    }
+    private ColorPicker makeColorPicker(String property, EventHandler<ActionEvent> handler){
+    	ColorPicker result = new ColorPicker();
+        result.setOnAction(handler);
+        result.getStyleClass().add("split-button");
+        result.setPromptText(property);
+        return result;
+    }
 
     public void backgroundColor () {
-        myCurrent = "black";
-        myCommandString.setCommand(myCurrent, 0);
-        System.out.println(myCurrent);
+        String myType = "Background Color ";
+    	changeColor(myType);
 
+    }
+    public void toggleGrid(){
+    	
     }
 
     public void changePenColor () {
-        System.out.println("black");
+    	    	
+    	String myType = "Pen Color ";
+        changeColor(myType);
 
     }
-
+    
+    public void changeColor(String myType){
+    	 Color c = colorPicker.getValue();
+         System.out.println("New Color's RGB = "+c.getRed()+" "+c.getGreen()+" "+c.getBlue());      
+         myCurrent = myType + c.toString();
+         myCommandString.setCommand(myCurrent,0);
+         System.out.println(myCurrent);
+    }
+    public void help(){
+    	
+    }
 }
