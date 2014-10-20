67ae12cceceb20f14c05cc4f2a4fd444d84347ba
Jesse Ling <jesse.ling@duke.edu>
2014-10-19 21:15:53 -0400

    Testing Key Pressed Implementations

--- src/view/TurtleControllerPane.java
+++ src/view/TurtleControllerPane.java
@@ -1,73 +1,104 @@
 package view;
 
 import java.io.File;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 
-
 public class TurtleControllerPane extends Pane {
-    private VBox myVbox = new VBox();
-    private TextField myMoveTextField = new TextField();
-    private TextField myAngleTextField = new TextField();
-    private CommandString myCommandString;
+	private VBox myVbox = new VBox();
+	private TextField myMoveTextField = new TextField();
+	private TextField myAngleTextField = new TextField();
+	private CommandString myCommandString;
 
-    /**
-     * Initializes the parameters of the InputPane.
-     *
-     * @param cs CommandString containing the String that represents the current command
-     */
-    public TurtleControllerPane (CommandString cs) {
-        myCommandString = cs;
-        myMoveTextField.setPrefColumnCount(5);
-        Button moveButton = makeButton("Forward", event -> move());
-        myAngleTextField.setPrefColumnCount(5);
-        Button angleButton = makeButton("Right (deg)", event -> angle());
-        Button chooseFileButton = makeButton("Choose Image", event -> doChoose());
-        myVbox.getChildren().addAll(chooseFileButton, myAngleTextField, angleButton,
-                                    myMoveTextField, moveButton);
-    }
+	/**
+	 * Initializes the parameters of the InputPane.
+	 *
+	 * @param cs
+	 *            CommandString containing the String that represents the
+	 *            current command
+	 */
+	public TurtleControllerPane(CommandString cs) {
+		myCommandString = cs;
+		myMoveTextField.setPrefColumnCount(5);
+		Button moveButton = makeButton("Forward", event -> move());
+		myAngleTextField.setPrefColumnCount(5);
+		Button angleButton = makeButton("Right (deg)", event -> angle());
+		Button chooseFileButton = makeButton("Choose Image",
+				event -> doChoose());
+		myVbox.getChildren().addAll(new Label("Turtle Commands"),
+				chooseFileButton, myAngleTextField, angleButton,
+				myMoveTextField, moveButton, new Label("Pen Commands"));
+		myVbox.setOnKeyPressed(new MoveHandler());
+	}
 
-    private Button makeButton (String property, EventHandler<ActionEvent> handler) {
-        Button result = new Button();
-        String label = property;
-        result.setText(label);
-        result.setOnAction(handler);
-        return result;
-    }
+	private Button makeButton(String property, EventHandler<ActionEvent> handler) {
+		Button result = new Button();
+		String label = property;
+		result.setText(label);
+		result.setOnAction(handler);
+		return result;
+	}
 
-    private void move () {
-        myCommandString.setCommand("Forward " + myMoveTextField.getText(), Constants.SETTING);
-        myMoveTextField.clear();
-    }
+	private void move() {
+		myCommandString.setCommand("Forward " + myMoveTextField.getText(),
+				Constants.SETTING);
+		myMoveTextField.clear();
+	}
 
-    private void angle () {
-        myCommandString.setCommand("Right " + myAngleTextField.getText(), Constants.SETTING);
-        myAngleTextField.clear();
+	private void angle() {
+		myCommandString.setCommand("Right " + myAngleTextField.getText(),
+				Constants.SETTING);
+		myAngleTextField.clear();
 
-    }
+	}
 
-    private void doChoose () {
-        final Label labelFile = new Label();
-        FileChooser fileChooser = new FileChooser();
-        // Set extension filter
-        FileChooser.ExtensionFilter extFilter =
-                new FileChooser.ExtensionFilter("Image Files (*.png)", "*.png");
-        fileChooser.getExtensionFilters().add(extFilter);
-        // Show open file dialog
-        File file = fileChooser.showOpenDialog(null);
-        labelFile.setText(file.getPath());
-    }
+	private void doChoose() {
+		final Label labelFile = new Label();
+		FileChooser fileChooser = new FileChooser();
+		// Set extension filter
+		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(
+				"Image Files (*.png)", "*.png");
+		fileChooser.getExtensionFilters().add(extFilter);
+		// Show open file dialog
+		File file = fileChooser.showOpenDialog(null);
+		labelFile.setText(file.getPath());
+	}
 
-    @Override
-    public BorderPane addPane (BorderPane p) {
-        p.setLeft(myVbox);
-        return p;
-    }
+	@Override
+	public BorderPane addPane(BorderPane p) {
+		p.setLeft(myVbox);
+		return p;
+	}
 
+	private void move(int orientation) {
+		myCommandString.setCommand("setheading " + orientation, 0);
+		myCommandString.setCommand("forward 90", 0);
+	}
+
+	private class MoveHandler implements EventHandler<KeyEvent> {
+		int orientation = 0;
+
+		@Override
+		public void handle(KeyEvent event) {
+			if (event.getCode() == KeyCode.UP) {
+				orientation = Constants.UP;
+			} else if (event.getCode() == KeyCode.RIGHT) {
+				orientation = Constants.RIGHT;
+			} else if (event.getCode() == KeyCode.DOWN) {
+				orientation = Constants.DOWN;
+			} else if (event.getCode() == KeyCode.LEFT) {
+				orientation = Constants.LEFT;
+			}
+			move(orientation);
+		}
+	}
 }
