b6786a62ad3bf518468c8995b512d64c451f093c
Martin Tamayo <mt153@duke.edu>
2014-10-21 21:09:33 -0400

    "NeoParser" should now be functional, though it could use some refactoring.

--- src/controller/MasterController.java
+++ src/controller/MasterController.java
@@ -10,8 +10,8 @@ import java.util.Set;
 import java.util.regex.Pattern;
 
 import javafx.scene.paint.Color;
-
-import parser.Parser;
+import parser.NeoParser;
+//import parser.Parser;
 
 /**
  * Class to contain the Parser and a set of executable commands.
@@ -26,39 +26,39 @@ public class MasterController {
     public static Map<String, String> myCommandMap;
     public static Map<String, Double> myVariableMap;
     public static Map<String, Pattern> myRegularExpressions;
-    protected static Parser myParser;
+    public static NeoParser myParser;
     private final String resources = "resources.languages/";
 
     public MasterController (String lang) {
-        myParser = new Parser();
+        myParser = new NeoParser();
         myColorMap = new HashMap<Integer, Color>();
         myCommandMap = new HashMap<String, String>();
         myVariableMap = new HashMap<String, Double>();
         myRegularExpressions = new HashMap<String, Pattern>();
         ResourceBundle language = ResourceBundle.getBundle(resources + lang);
-        Set<String> expressions = constructSet();
+//        Set<String> expressions = constructSet();
         Enumeration<String> enumerator = language.getKeys();
         while (enumerator.hasMoreElements()) {
             String command = (String) enumerator.nextElement();
             String[] inputs = language.getString(command).split(",");
-            if(expressions.contains(command)){
-            	myRegularExpressions.put(command, Pattern.compile(inputs[0]));
-            }
-            else{
+//            if(expressions.contains(command)){
+//            	myRegularExpressions.put(command, Pattern.compile(inputs[0]));
+//            }
+//            else{
             	command = "commands." + command + "Command";
             	for (String input : inputs) {
             		myCommandMap.put(input, command);
-            	}
+//            	}
             }
         }
     }
     
-    private Set<String> constructSet() {
-		Set<String> expressionSet = new HashSet<String>();
-		String[] expressionArray = {"Comment", "Constant", "Variable", "Command", "ListStart", "ListEnd", "GroupStart", "GroupEnd"};
-		for(String item : expressionArray){
-			expressionSet.add(item);
-		}
-		return expressionSet;
-	}
+//    private Set<String> constructSet() {
+//		Set<String> expressionSet = new HashSet<String>();
+//		String[] expressionArray = {"Comment", "Constant", "Variable", "Command", "ListStart", "ListEnd", "GroupStart", "GroupEnd"};
+//		for(String item : expressionArray){
+//			expressionSet.add(item);
+//		}
+//		return expressionSet;
+//	}
 }
