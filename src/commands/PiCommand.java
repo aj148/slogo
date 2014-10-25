e37be9cc6bcfe0e2ede74d2b0cc3774200fdcfea
Martin Tamayo <mt153@duke.edu>
2014-10-25 19:23:52 -0400

    Added Pi Command.

---
+++ src/commands/PiCommand.java
@@ -0,0 +1,11 @@
+package commands;
+
+import model.Model;
+
+public class PiCommand extends ZeroInputCommand {
+	
+	@Override
+    public double executeCommand(Model model) {
+		return Math.PI;
+	}
+}
