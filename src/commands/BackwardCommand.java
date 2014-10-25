d0844dc37365fd69718e22b44a0bf641c04afe99
Martin Tamayo <mt153@duke.edu>
2014-10-25 14:32:58 -0400

    Still trying to fix compiler issues.

--- src/commands/BackwardCommand.java
+++ src/commands/BackwardCommand.java
@@ -8,6 +8,5 @@ public class BackwardCommand extends OneInputCommand {
     @Override
     public double updateTurtle (Model model, Turtle a) {
         return a.updatePosition(-myParameters[0].executeCommand(model));
-
     }
 }
