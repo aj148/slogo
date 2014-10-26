096dbbcc712266fc6cd5fd697741d984e6bdd456
Minkwon Lee <ml241@duke.edu>
2014-10-25 23:24:43 -0400

    reversing the order of commands and throwing errors in controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -45,12 +45,12 @@ public class Controller {
      * @param input : User input string from the ViewPanel.
      */
     public void getInput (String input) {
-        List<Command> commandsToExecute = MasterController.myParser.parseInput(input);
-//        if(!commandsToExecute.isEmpty() && commandsToExecute.peek().getClassName().equals("commands.ErrorCommand")){
-//        	ErrorCommand error = (ErrorCommand)commandsToExecute.pop();
-//            myPane.showError(error.showError());
-//        	return;
-//        }
+        Stack<Command> commandsToExecute = MasterController.myParser.parseInput(input);
+        if(!commandsToExecute.isEmpty() && commandsToExecute.peek().getClassName().equals("commands.ErrorCommand")){
+        	ErrorCommand error = (ErrorCommand)commandsToExecute.pop();
+            myPane.showError(error.showError());
+        	return;
+        }
         runCommand(commandsToExecute);
     }
 
