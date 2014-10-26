3d9c9b90c333f4d2e1d386cdd1437d3b0029e75d
Ben Reisner <Benjamin.Reisner@duke.edu>
2014-10-26 03:42:31 -0400

    Fixed home

--- src/commands/ClearScreenCommand.java
+++ src/commands/ClearScreenCommand.java
@@ -6,7 +6,7 @@ public class ClearScreenCommand extends ZeroInputCommand {
 	
 	@Override
 	public double executeCommand(Model model) {
-		// STILL need signal to clear screen.
+		
 		Command a = new HomeCommand();
 		return a.executeCommand(model);
 	}
--- src/commands/HomeCommand.java
+++ src/commands/HomeCommand.java
@@ -7,6 +7,7 @@ public class HomeCommand extends ZeroInputCommand {
 	
 	@Override
     public double updateTurtle(Model model, Turtle a) {
+		a.setHeading(0.0);
         return a.setXAndY(0.0, 0.0);
     }
 }
